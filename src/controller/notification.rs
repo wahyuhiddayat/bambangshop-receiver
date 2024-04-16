e8591039bd8f0c2b180e3627ee8ee8b6b0a1557e
Wahyu Hidayat <wahyuhidayaat510@gmail.com>
2024-04-16 20:43:51 +0700

    Implement the receive function in Notification controller.

--- src/controller/notification.rs
+++ src/controller/notification.rs
@@ -21,6 +21,14 @@ pub fn unsubscribe(product_type: &str) -> Result<Json<SubscriberRequest>> {
     };
 }
 
+#[post("/receive", data = "<notification>")]
+pub fn receive(notification: Json<Notification>) -> Result<Json<Notification>> {
+    return match NotificationService::receive_notification(notification.into_inner()) {
+        Ok(f) => Ok(Json::from(f)),
+        Err(e) => Err(e)
+    };
+}
+
 #[get("/")]
 pub fn list() -> Result<Json<Vec<String>>> {
     return match NotificationService::list_messages() {
